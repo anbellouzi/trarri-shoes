00d092252683b64e61b6ca93e4d2aae964fd9368
Anas Bellouzi <anas.bellouzi@students.makeschool.com>
2019-11-23 18:09:10 -0800

    fixed mobile navbar

--- static/script.js
+++ static/script.js
@@ -16,29 +16,28 @@ let counter = 0
 window.onscroll = function() {
     var nav = document.getElementById('navbar');
     if ( window.pageYOffset > 200 ) {
+        // small nav
         nav.classList.remove("bg-transparent");
         nav.classList.add("fixed");
         nav.classList.add("nav-bg-yeezy");
         if (counter == 1) {
           var selectedClass = "";
           selectedClass = $(this).attr("data-rel");
-          $("#navbar").fadeTo(100, 0.1);
+          $("#navbar").fadeTo(100, 0.8);
           setTimeout(function() {
-            $("#navbarNav").fadeIn().addClass('animation');
-            $("#navbar").fadeTo(50, 1);
+            $("#navbar").fadeIn().addClass('animation');
+            $("#navbar").fadeTo(100, 1);
           }, 300);
         }
 
         counter += 1
 
     } else {
-
+        // big nav
         nav.classList.remove("nav-bg-yeezy");
-        // nav.classList.add("bg-transparent");
         counter = 0
         setTimeout(function() {
-          $("#navbarNav").fadeOut().addClass('animation');
-          // $("#navbar").fadeTo(300, 1);
+          $("#navbar").fadeIn("slow").addClass('animation');
         }, 300);
         nav.classList.remove("fixed");
     }
